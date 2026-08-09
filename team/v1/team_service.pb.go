6c168d4d1f08c5b5d931b8fa77ae515cd2231699
Junaidmdv <juni313786@gmail.com>
2026-08-09 23:07:19 +0530

    fix:remove the team id from updateTeam

--- team/v1/team_service.pb.go
+++ team/v1/team_service.pb.go
@@ -782,14 +782,13 @@ func (x *CreateTeamRes) GetTeamMember() *TeamMemberDetails {
 
 type UpdateTeamReq struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
-	TeamId        string                 `protobuf:"bytes,1,opt,name=team_id,json=teamId,proto3" json:"team_id,omitempty"`
-	UserId        string                 `protobuf:"bytes,2,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
-	Name          *string                `protobuf:"bytes,3,opt,name=name,proto3,oneof" json:"name,omitempty"`
-	City          *string                `protobuf:"bytes,4,opt,name=city,proto3,oneof" json:"city,omitempty"`
-	Description   *string                `protobuf:"bytes,5,opt,name=description,proto3,oneof" json:"description,omitempty"`
-	ShortName     *string                `protobuf:"bytes,6,opt,name=short_name,json=shortName,proto3,oneof" json:"short_name,omitempty"`
-	Email         *string                `protobuf:"bytes,7,opt,name=email,proto3,oneof" json:"email,omitempty"`
-	PhoneNumber   *string                `protobuf:"bytes,8,opt,name=phone_number,json=phoneNumber,proto3,oneof" json:"phone_number,omitempty"`
+	UserId        string                 `protobuf:"bytes,1,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
+	Name          *string                `protobuf:"bytes,2,opt,name=name,proto3,oneof" json:"name,omitempty"`
+	City          *string                `protobuf:"bytes,3,opt,name=city,proto3,oneof" json:"city,omitempty"`
+	Description   *string                `protobuf:"bytes,4,opt,name=description,proto3,oneof" json:"description,omitempty"`
+	ShortName     *string                `protobuf:"bytes,5,opt,name=short_name,json=shortName,proto3,oneof" json:"short_name,omitempty"`
+	Email         *string                `protobuf:"bytes,6,opt,name=email,proto3,oneof" json:"email,omitempty"`
+	PhoneNumber   *string                `protobuf:"bytes,7,opt,name=phone_number,json=phoneNumber,proto3,oneof" json:"phone_number,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -822,13 +821,6 @@ func (x *UpdateTeamReq) ProtoReflect() protoreflect.Message {
 // Deprecated: Use UpdateTeamReq.ProtoReflect.Descriptor instead.
 func (*UpdateTeamReq) Descriptor() ([]byte, []int) {
 	return file_team_service_proto_rawDescGZIP(), []int{6}
-}
-
-func (x *UpdateTeamReq) GetTeamId() string {
-	if x != nil {
-		return x.TeamId
-	}
-	return ""
 }
 
 func (x *UpdateTeamReq) GetUserId() string {
@@ -3547,17 +3539,16 @@ const file_team_service_proto_rawDesc = "" +
 	"\x04user\x18\x01 \x01(\v2\r.team.UserResR\x04user\x127\n" +
 	"\fteam_details\x18\x02 \x01(\v2\x14.team.TeamDetailsResR\vteamDetails\x128\n" +
 	"\vteam_member\x18\x03 \x01(\v2\x17.team.TeamMemberDetailsR\n" +
-	"teamMember\"\xcd\x02\n" +
+	"teamMember\"\xb4\x02\n" +
 	"\rUpdateTeamReq\x12\x17\n" +
-	"\ateam_id\x18\x01 \x01(\tR\x06teamId\x12\x17\n" +
-	"\auser_id\x18\x02 \x01(\tR\x06userId\x12\x17\n" +
-	"\x04name\x18\x03 \x01(\tH\x00R\x04name\x88\x01\x01\x12\x17\n" +
-	"\x04city\x18\x04 \x01(\tH\x01R\x04city\x88\x01\x01\x12%\n" +
-	"\vdescription\x18\x05 \x01(\tH\x02R\vdescription\x88\x01\x01\x12\"\n" +
+	"\auser_id\x18\x01 \x01(\tR\x06userId\x12\x17\n" +
+	"\x04name\x18\x02 \x01(\tH\x00R\x04name\x88\x01\x01\x12\x17\n" +
+	"\x04city\x18\x03 \x01(\tH\x01R\x04city\x88\x01\x01\x12%\n" +
+	"\vdescription\x18\x04 \x01(\tH\x02R\vdescription\x88\x01\x01\x12\"\n" +
 	"\n" +
-	"short_name\x18\x06 \x01(\tH\x03R\tshortName\x88\x01\x01\x12\x19\n" +
-	"\x05email\x18\a \x01(\tH\x04R\x05email\x88\x01\x01\x12&\n" +
-	"\fphone_number\x18\b \x01(\tH\x05R\vphoneNumber\x88\x01\x01B\a\n" +
+	"short_name\x18\x05 \x01(\tH\x03R\tshortName\x88\x01\x01\x12\x19\n" +
+	"\x05email\x18\x06 \x01(\tH\x04R\x05email\x88\x01\x01\x12&\n" +
+	"\fphone_number\x18\a \x01(\tH\x05R\vphoneNumber\x88\x01\x01B\a\n" +
 	"\x05_nameB\a\n" +
 	"\x05_cityB\x0e\n" +
 	"\f_descriptionB\r\n" +
